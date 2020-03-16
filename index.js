8885985e1e099fb4e0591f7151e2c4dc97076b2f
Justin Perez <justin@startrev.com>
2020-03-15 23:24:20 -0700

    Add install route

--- index.js
+++ index.js
@@ -16,6 +16,25 @@ app.get('/', (req, res) => {
   res.send('Hello Justin!');
 });
 
+// Shopify install route
+app.get('/shopify', (req, res) => {
+  const shop = req.query.shop;
+  if (shop) {
+    const state = nonce();
+    const redirectUri = forwardingAddress + '/shopify/callback';
+    const installUrl = 'https://' + shop +
+      '/admin/oauth/authorize?client_id=' + apiKey +
+      '&scope=' + scopes +
+      '&state=' + state +
+      '&redirect_uri=' + redirectUri;
+
+    res.cookie('state', state);
+    res.redirect(installUrl);
+  } else {
+    return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+  }
+});
+
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
